8fb56e6279b4258667367a3f9c3d558c57226197
wamakimaN <wamamoses@gmail.com>
2019-10-25 15:59:17 +0300

    chang if else statement and declare gender variable separately

--- scripts.js
+++ scripts.js
@@ -4,16 +4,17 @@ function check() {
   var century = parseInt(document.getElementById("century").value);
   var year = parseInt(document.getElementById("year").value);
   var month = parseInt(document.getElementById("month").value);
+  var gender = document.getElementById("gender").value;
 
   var findDay = Math.round((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7)
   var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
   var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 
-  if (document.getElementById("male").checked) {
+  if (gender === "male") {
     alert("Your Akan name is " + maleName[findDay]);
-  } else if (document.getElementById("female").checked) {
+  } else if (gender === "female") {
     alert("Your Akan name is " + femaleName[findDay]);
-  } else {
-    return false
   };
+
 }
+//ui logic
