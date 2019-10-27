985baa44f534042e4e58dc3f28368250c61ca214
wamakimaN <wamamoses@gmail.com>
2019-10-27 15:43:21 +0300

    change the arrangement of my arrays to mathc days starting from saturday

--- scripts.js
+++ scripts.js
@@ -6,10 +6,13 @@ function check() {
   var month = parseInt(document.getElementById("month").value);
   var gender = document.getElementById("gender").value;
 
-  var findDay = Math.round((((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7)
-  var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-  var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+  var findDay = Math.floor((century / 4 - 2 * century - 1 + (5 * year )/ 4 + (26 * (month + 1)) / 10 + day) % 7)
 
+  //here we match the days starting from Saturday with the corresponding name.
+  //Amma is hence Saturday and Afua is Friday.
+  var femaleName = ["Amma","Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"]
+  var maleName = ["Kwame","Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"]
+alert(findDay)
   if (gender === "male") {
     alert("Your Akan name is " + maleName[findDay]);
   } else if (gender === "female") {
