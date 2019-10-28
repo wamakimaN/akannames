c1a01ea7b5dd9b5e86c30ccd2c9ed95772a0b030
wamakimaN <wamamoses@gmail.com>
2019-10-28 08:36:21 +0300

    remove any unnecessary lines of code

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 //business logic
 function check() {
-  var day = parseInt(document.getElementById("date").value);
-  var century = parseInt(document.getElementById("century").value);
-  var year = parseInt(document.getElementById("year").value);
-  var month = parseInt(document.getElementById("month").value);
+  var day = document.getElementById("date").value;
+  var century = document.getElementById("century").value;
+  var year = document.getElementById("year").value;
+  var month = document.getElementById("month").value;
   var gender = document.getElementById("gender").value;
 
   var findDay = Math.floor((century / 4 - 2 * century - 1 + (5 * year )/ 4 + (26 * (month + 1)) / 10 + day) % 7)
@@ -12,7 +12,7 @@ function check() {
   //Amma is hence Saturday and Afua is Friday.
   var femaleName = ["Amma","Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"]
   var maleName = ["Kwame","Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"]
-alert(findDay)
+
   if (gender === "male") {
     alert("Your Akan name is " + maleName[findDay]);
   } else if (gender === "female") {
