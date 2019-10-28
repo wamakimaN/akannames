f00830822554453996340880fe2b191a2bca529c
wamakimaN <wamamoses@gmail.com>
2019-10-28 17:44:26 +0300

    redo business logig inorder to use a class and its prototype

--- scripts.js
+++ scripts.js
@@ -1,26 +1,18 @@
 //business logic
-function check() {
-  var day = document.getElementById("date").value;
-  var century = document.getElementById("century").value;
-  var year = document.getElementById("year").value;
-  var month = document.getElementById("month").value;
-  var gender = document.getElementById("gender").value;
+//global variables
+var femaleName = ["Amma", "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"]
+var maleName = ["Kwame", "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"]
 
-  if (day >= 1 && day <= 31 && century <= 20 && year <= 99 && month >= 1 && month <= 12) {
-    var findDay = Math.floor((century / 4 - 2 * century - 1 + (5 * year) / 4 + (26 * (month + 1)) / 10 + day) % 7)
-  } else {
-    return false;
+class BirthDay {
+  constructor(century, year, month, day) {
+    this.century = century;
+    this.year = year;
+    this.month = month;
+    this.day = day;
   }
-  //here we match the days starting from Saturday with the corresponding name.
-  //Amma is hence Saturday and Afua is Friday.
-  var femaleName = ["Amma", "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"]
-  var maleName = ["Kwame", "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"]
-
-  if (gender === "male") {
-    alert("Your Akan name is " + maleName[findDay]);
-  } else if (gender === "female") {
-    alert("Your Akan name is " + femaleName[findDay]);
-  };
-
+  findDay() {
+    return Math.floor((this.century / 4 - 2 * this.century - 1 + (5 * this.year) / 4 + (26 * (this.month + 1)) / 10 + this.day) % 7)
+  }
 }
+
 //ui logic
